e18460b1a8252540041b75375096e23a64667096
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-14 11:50:34 -0300

    feat: insert state in login

--- src/shared/inputs/input/Input.tsx
+++ src/shared/inputs/input/Input.tsx
@@ -6,11 +6,12 @@ import { BoxInput, TitleInput } from './input.styled';
 
 interface InputProps extends InputPropsAntd {
     title?: string;
+    margin?: string;
 }
 
-const Input = ({ title, ...props }: InputProps) => {
+const Input = ({ title, margin, ...props }: InputProps) => {
     return (
-    <BoxInput>
+    <BoxInput style={{ margin }}>
         {title && <TitleInput>{title}</TitleInput>}
         <InputAntd {...props} />
     </BoxInput>
