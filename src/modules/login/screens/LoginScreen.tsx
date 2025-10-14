fdb2f12a1c3d7a8aa9466b9786fb747fc238ad33
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-14 09:18:28 -0300

    feat: create login component

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,11 +1,16 @@
-import { BackgroundImage } from "../styles/loginScreen.styles";
+import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage } from "../styles/loginScreen.styles";
 
 
 const LoginScreen = () => {
     return (
-    <div>
+    <ContainerLoginScreen>
+        <ContainerLogin>
+            <LimitedContainer>
+                <LogoImage src="./logo.png" />
+            </LimitedContainer>
+        </ContainerLogin>
         <BackgroundImage src="./background.png"/>
-    </div>
+    </ContainerLoginScreen>
     );
 };
 
