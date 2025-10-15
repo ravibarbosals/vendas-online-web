508496c174940de84e35c4e1e05b8873f28a7652
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-15 12:41:28 -0300

    feat: correction <T> in last class

--- src/shared/functions/connection/connectionAPI.ts
+++ src/shared/functions/connection/connectionAPI.ts
@@ -3,23 +3,25 @@ import { ERROR_ACCES_DANIED, ERROR_CONNECTION } from "../../constants/errorsStat
 import { MethodsEnum } from "../../enums/methods.enum";
 
 export default class ConnectionAPI {
-    static async call<T>(url: string, method: string, body?: unknown) {
+    static async call<T>(url: string, method: string, body?: unknown): Promise<T> {
         switch(method) {
             case MethodsEnum.GET:
-                return (await axios.get<T>(url)).data ;
+                return (await axios.get<T>(url)).data;
             case MethodsEnum.POST:
-                return (await axios.post<T>(url, body)).data ;
+                return (await axios.post<T>(url, body)).data;
             case MethodsEnum.DELETE:
-                return (await axios.delete<T>(url)).data ;
+                return (await axios.delete<T>(url)).data;
             case MethodsEnum.PATCH:
-                return (await axios.patch<T>(url, body)).data ;
+                return (await axios.patch<T>(url, body)).data;
             case MethodsEnum.PUT:
-                return (await axios.put<T>(url, body)).data ;
+                return (await axios.put<T>(url, body)).data;
+            default:
+                return (await axios.patch<T>(url, body)).data;
         }
     }
 
-    static async connect(url: string, method: string, body?: unknown) {
-    return ConnectionAPI.call(url, method, body).catch((error) => {
+    static async connect<T>(url: string, method: string, body?: unknown): Promise<T> {
+    return ConnectionAPI.call<T>(url, method, body).catch((error) => {
         if (error.response) {
             switch (error.response.status) {
                 case 401:
@@ -29,26 +31,28 @@ export default class ConnectionAPI {
                     throw new Error(ERROR_CONNECTION);
             }
         }
+        throw new Error(ERROR_CONNECTION);
+
     });
    }
 }
 
-export const ConnectionAPIGet = async (url: string) => {
-    return ConnectionAPI.connect(url, MethodsEnum.GET);
+export const ConnectionAPIGet = async <T>(url: string): Promise<T> => {
+    return ConnectionAPI.connect<T>(url, MethodsEnum.GET);
 };
 
-export const ConnectionAPIPost = async (url: string , body: unknown) => {
-    return ConnectionAPI.connect(url, MethodsEnum.POST, body);
+export const ConnectionAPIPost = async <T>(url: string , body: unknown): Promise<T> => {
+    return ConnectionAPI.connect<T>(url, MethodsEnum.POST, body);
 };
 
-export const ConnectionAPIDelete = async (url: string) => {
-    return ConnectionAPI.connect(url, MethodsEnum.DELETE);
+export const ConnectionAPIDelete = async <T>(url: string): Promise<T> => {
+    return ConnectionAPI.connect<T>(url, MethodsEnum.DELETE);
 };
 
-export const ConnectionAPIPatch = async (url: string, body: unknown) => {
-    return ConnectionAPI.connect(url, MethodsEnum.PATCH,  body);
+export const ConnectionAPIPatch = async <T>(url: string, body: unknown): Promise<T> => {
+    return ConnectionAPI.connect<T>(url, MethodsEnum.PATCH,  body);
 };
 
-export const ConnectionAPIPut = async (url: string, body: unknown) => {
-    return ConnectionAPI.connect(url, MethodsEnum.PUT,  body);
+export const ConnectionAPIPut = async <T>(url: string, body: unknown): Promise<T> => {
+    return ConnectionAPI.connect<T>(url, MethodsEnum.PUT,  body);
 };
