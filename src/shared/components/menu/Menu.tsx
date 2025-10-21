6fac71d5c9383947a650e3c00a328e9a35814f15
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-21 13:13:47 -0300

    feat: nsert navigate menu

--- src/shared/components/menu/Menu.tsx
+++ src/shared/components/menu/Menu.tsx
@@ -1,12 +1,93 @@
 import { ContainerLogoName, ContainerMenu, LogoMenu, NameCompany } from "./menu.style";
+import React, { useState } from 'react';
+import { HomeOutlined, LaptopOutlined, ProfileOutlined, SafetyCertificateOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps,  } from 'antd';
+import { Menu as MenuAntd,  } from 'antd';
+import { useNavigate } from "react-router-dom";
+import { ProductRoutesEnum } from "../../../modules/product/routes";
+
+type MenuItem = Required<MenuProps>['items'][number];
 
 const Menu = () => {
+    const navigate = useNavigate();
+    const [current, setCurrent] = useState('1');
+
+    const items: MenuItem[] = [
+        {
+        key: 'home',
+        label: 'Principal',
+        icon: <HomeOutlined/>,
+        },
+        {
+        key: 'products',
+        label: 'Produtos',
+        icon: <LaptopOutlined />,
+        children: [
+            { 
+                key: 'products_view', 
+                label: 'Visualizar',
+                onClick: () => navigate(ProductRoutesEnum.PRODUCT),
+            },
+            { 
+                key: 'products_insert', 
+                label: 'Inserir',
+                onClick: () => navigate(ProductRoutesEnum.PRODUCT_INSERT),
+            },
+        ],
+        },
+
+        {
+        key: 'categories',
+        label: 'Categoria',
+        icon: <ProfileOutlined />,
+        children: [
+            { 
+                key: 'category_view', 
+                label: 'Visualizar',
+                onClick: () => navigate(ProductRoutesEnum.PRODUCT),
+            },
+            { 
+                key: 'category_insert', 
+                label: 'Inserir',
+                onClick: () => navigate(ProductRoutesEnum.PRODUCT_INSERT),
+            },
+        ],
+        },
+        {
+        key: 'order',
+        label: 'Pedidos',
+        icon: <SafetyCertificateOutlined/>,
+        },
+        {
+        key: 'user',
+        label: 'Clientes',
+        icon: <UserOutlined/>,
+        },
+
+        
+    
+        
+];
+
+    const onClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key);
+    };
+
     return (
     <ContainerMenu>
         <ContainerLogoName>
         <LogoMenu/>
     <NameCompany>Vendas Online</NameCompany>
         </ContainerLogoName>
+        <MenuAntd
+        theme="dark"
+        onClick={onClick}
+        style={{ width: 240 }}
+        defaultOpenKeys={['sub1']}
+        selectedKeys={[current]}
+        mode="inline"
+        items={items}
+      />
     </ContainerMenu>
     );
 };
