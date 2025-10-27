641549befada1cd93f197f299f39c569b367548d
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-27 13:05:57 -0300

    feat: insert unit test in money and localStorage

---
+++ src/shared/functions/__tests__/money.spec.ts
@@ -0,0 +1,23 @@
+import { convertNumberToMoney } from '../money';
+
+describe('money', () => {
+  it('should return cents', () => {
+    const returnValue = convertNumberToMoney(543.43);
+
+    expect(returnValue).toContain('R$');
+    expect(returnValue).toContain('543,43');
+  });
+
+  it('should return integer', () => {
+    const returnValue = convertNumberToMoney(643);
+
+    expect(returnValue).toContain('R$');
+    expect(returnValue).toContain('643,00');
+  });
+  it('should return thousand', () => {
+    const returnValue = convertNumberToMoney(434344.43);
+
+    expect(returnValue).toContain('R$');
+    expect(returnValue).toContain('434.344,43');
+  });
+});
