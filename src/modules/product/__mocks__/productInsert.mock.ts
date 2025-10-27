5bf47a556397b3ab4ad22afb49392be9645b7e7b
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-27 10:32:56 -0300

    feat: insert unit test in insertProduct

--- src/modules/product/__mocks__/productInsert.mock.ts
+++ src/modules/product/__mocks__/productInsert.mock.ts
@@ -3,5 +3,5 @@ import { InsertProduct } from '../../../shared/dtos/InsertProduct.dto';
 export const mockProductInsert: InsertProduct = {
   image: 'http://image',
   name: 'MOCK NAME',
-  price: 543,
+  price: 543.43,
 };
