1532863749aa8ea9f96d90955b22d09881d6f37c
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-27 11:35:48 -0300

    feat: insert unit test in useInsertProduct

---
+++ src/modules/product/hooks/__tests__/useInsertProduct.spec.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { act, renderHook } from '@testing-library/react';
+
+import { userInsertProduct } from '../useInsertProduct';
+
+const mockNavigate = jest.fn();
+const mockSetNotification = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  useNavigate: () => mockNavigate,
+}));
+
+jest.mock('../../../../store/reducers/globalReducer/useGlobalReducer', () => ({
+  useGlobalReducer: () => ({
+    setNotification: mockSetNotification,
+  }),
+}));
+
+describe('Test useInsertProduct', () => {
+  it('should return initial values', () => {
+    const { result } = renderHook(() => userInsertProduct());
+
+    expect(result.current.loading).toEqual(false);
+    expect(result.current.disableButton).toEqual(true);
+    expect(result.current.product).toEqual({
+      name: '',
+      price: 0,
+      image: '',
+    });
+  });
+
+  it('should change select in handleChangeSelect', () => {
+    const { result } = renderHook(() => userInsertProduct());
+
+    act(() => {
+      result.current.handleChangeSelect('4344');
+    });
+
+    expect(result.current.product.categoryId).toEqual(4344);
+  });
+
+  it('should change product in onChangeInput send name', () => {
+    const TEST_MOCK = 'TEST_MOCK';
+    const { result } = renderHook(() => userInsertProduct());
+
+    act(() => {
+      result.current.onChangeInput({ target: { value: TEST_MOCK } } as any, 'name');
+    });
+
+    expect(result.current.product.name).toEqual(TEST_MOCK);
+  });
+  it('should change product in onChangeInput send price', () => {
+    const TEST_MOCK = '3453';
+    const { result } = renderHook(() => userInsertProduct());
+
+    act(() => {
+      result.current.onChangeInput({ target: { value: TEST_MOCK } } as any, 'price', true);
+    });
+
+    expect(result.current.product.price).toEqual(Number(TEST_MOCK));
+  });
+});
