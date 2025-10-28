ee29e1d06db833275e5d58a94d97eb8b75ba740f
Ravi Almeida Barbosa Lima <ravialmeida10@gmail.com>
2025-10-28 16:15:27 -0300

    feat: create order screen

--- src/modules/orders/screens/Order.tsx
+++ src/modules/orders/screens/Order.tsx
@@ -1,6 +1,10 @@
 import Screen from '../../../shared/components/screen/Screen';
+import { useOrder } from '../hooks/useOrder';
 
 const Order = () => {
+  const { orders } = useOrder();
+
+  console.log(orders);
   return (
     <Screen>
       <div>Orders</div>
