e4c8cd8fc820d6ea9668eeb7787ecac6b370e5e3
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-30 11:41:31 -0300

    feat: create screen order detail

--- src/modules/orders/routes.tsx
+++ src/modules/orders/routes.tsx
@@ -1,14 +1,20 @@
 import { type RouteObject } from 'react-router-dom';
 
 import Order from './screens/Order';
+import OrderDetail from './screens/OrderDetail';
 
-export enum OrdersRoutesEnum {
+export enum OrderRoutesEnum {
   ORDER = '/order',
+  ORDER_ID = '/order/id',
 }
 
 export const orderScreens: RouteObject[] = [
   {
-    path: OrdersRoutesEnum.ORDER,
+    path: OrderRoutesEnum.ORDER,
     element: <Order />,
+  },
+  {
+    path: OrderRoutesEnum.ORDER_ID,
+    element: <OrderDetail />,
   },
 ];
---
+++ src/modules/orders/screens/OrderDetail.tsx
@@ -0,0 +1,152 @@
+import type { DescriptionsProps } from 'antd';
+import { Descriptions, Divider } from 'antd';
+import React from 'react';
+
+import Screen from '../../../shared/components/screen/Screen';
+import { OrderRoutesEnum } from '../routes';
+
+const items: DescriptionsProps['items'] = [
+  {
+    key: '1',
+    label: 'Nome',
+    children: 'Ravi',
+  },
+  {
+    key: '2',
+    label: 'Email',
+    children: 'Prepeaid',
+    span: 2,
+  },
+  {
+    key: '3',
+    label: 'Telefone',
+    children: 'YES',
+  },
+  {
+    key: '4',
+    label: 'CPF',
+    children: '2018-04-24 18:00:00',
+    span: 2,
+  },
+];
+
+const items2: DescriptionsProps['items'] = [
+  {
+    key: '1',
+    label: 'Nome',
+    children: 'Ravi',
+  },
+  {
+    key: '2',
+    label: 'Email',
+    children: 'Prepeaid',
+    span: 2,
+  },
+  {
+    key: '3',
+    label: 'Telefone',
+    children: 'YES',
+  },
+  {
+    key: '4',
+    label: 'CPF',
+    children: '2018-04-24 18:00:00',
+    span: 2,
+  },
+  {
+    key: '5',
+    label: 'Usage Time',
+    children: '2019-04-24 18:00:00',
+    span: 2,
+  },
+];
+
+const items3: DescriptionsProps['items'] = [
+  {
+    key: '1',
+    label: 'Nome',
+    children: 'Ravi',
+  },
+  {
+    key: '2',
+    label: 'Email',
+    children: 'Prepeaid',
+    span: 2,
+  },
+  {
+    key: '3',
+    label: 'Telefone',
+    children: 'YES',
+  },
+  {
+    key: '4',
+    label: 'CPF',
+    children: '2018-04-24 18:00:00',
+    span: 2,
+  },
+  {
+    key: '5',
+    label: 'Usage Time',
+    children: '2019-04-24 18:00:00',
+    span: 2,
+  },
+];
+const items4: DescriptionsProps['items'] = [
+  {
+    key: '1',
+    label: 'Nome',
+    children: 'Ravi',
+  },
+  {
+    key: '2',
+    label: 'Email',
+    children: 'Prepeaid',
+    span: 2,
+  },
+  {
+    key: '3',
+    label: 'Telefone',
+    children: 'YES',
+  },
+  {
+    key: '4',
+    label: 'CPF',
+    children: '2018-04-24 18:00:00',
+    span: 2,
+  },
+  {
+    key: '5',
+    label: 'Usage Time',
+    children: '2019-04-24 18:00:00',
+    span: 2,
+  },
+];
+
+const OrderDetail: React.FC = () => {
+  return (
+    <Screen
+      listBreadcrumb={[
+        {
+          name: 'HOME',
+        },
+        {
+          name: 'PEDIDOS',
+          navigateTo: OrderRoutesEnum.ORDER,
+        },
+        {
+          name: 'DETALHES',
+        },
+      ]}
+    >
+      <Descriptions title="Dados do Usuário" bordered items={items} />
+      <Divider />
+      <Descriptions title="Dados do pagamento" bordered items={items2} />
+      <Divider />
+      <Descriptions title="Dados do endereço" bordered items={items3} />
+      <Divider />
+      <Descriptions title="Produtos" bordered items={items4} />
+    </Screen>
+  );
+};
+
+export default OrderDetail;
